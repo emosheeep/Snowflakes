9a0737e9b288f41ddd555fe9f4c18d6ac2cc1c9a
QinXuYang <1446445040@qq.com>
2020-01-11 00:33:43 +0800

    新增雪花颜色选择，可以传入数组，更新readme

--- js/snow.js
+++ js/snow.js
@@ -1,7 +1,9 @@
-const random = function (min, max) {
+const random = function (min, max, floor = false) {
     if (max < min) {
         throw new Error(`min is ${min}, max is ${max}, max should bigger than min!`)
-    } else {
+    } else if (floor) {
+		return Math.floor(min + Math.random() * (max - min))
+	} else {
         return min + Math.random() * (max - min)
     }
 }
@@ -19,20 +21,21 @@ class Snowflake {
         this.x = random(0, window.innerWidth)
         this.y = random(-window.innerHeight, 0)
         this.angle = 0
-
         this.va = Math.PI / random(...this.config.va) // 角速度
         this.va = Math.random() >= 0.5 ?  this.va : -this.va // 旋转方向
         this.vx = random(...this.config.vx)
         this.vy = random(...this.config.vy)
         this.alpha = random(...this.config.alpha) // 透明度
         this.radius = random(...this.config.radius) // 大小
+		let {color} = this.config
+		this.color = color[random(0, color.length, true)]
     }
     update(range) {
         this.x += this.vx
         this.y += this.vy
         this.angle += this.va
 
-        if (this.y > range ) {
+        if (this.y >= range + this.radius ) {
             this.load()
         }
     }
@@ -40,21 +43,21 @@ class Snowflake {
 // 负责创建雪花，重绘画面
 class Snow {
     constructor(container, config = {}) {
-        config = {
-			color: config.color || 'white',
-            image: config.image,
+        this.config = {
+            image: config.image || '',
             num: config.num || window.innerWidth / 2,
             snowflakes: Object.assign({
+                color: config.color || ['white'],
                 vx: [-3, 3],
                 vy: [2, 5],
-                va: [45, 360],
+                va: [45, 180],
                 radius: [5, 15],
                 alpha: [0.1, 0.9]
             }, config.snowflakes)
         }
-        this.init(container, config)
+        this.init(container)
     }
-    init (container, config) {
+    init (container) {
         // 初始化基本配置
         this.container = document.querySelector(container)
         this.canvas = document.createElement('canvas')
@@ -64,30 +67,31 @@ class Snow {
         this.container.appendChild(this.canvas)
         this.snowflakes = new Set()
 
-        for (let i = 0; i < config.num; i++) {
-            this.snowflakes.add(new Snowflake(config.snowflakes))
+        for (let i = 0; i < this.config.num; i++) {
+            this.snowflakes.add(new Snowflake(this.config.snowflakes))
         }
         // 画图或画圆
-        if (config.image) {
+        if (this.config.image) {
             this.img = new Image()
-            this.img.src = config.image
+            this.img.src = this.config.image
             this.img.onload = () => {
                 requestAnimationFrame(this.drawPicture())
             }
         } else {
-            requestAnimationFrame(this.drawCircle(config.color))
+            requestAnimationFrame(this.drawCircle())
         }
     }
     // 返回一个帧动画函数
-    drawCircle (color) {
+    drawCircle () {
         // 利用箭头函数绑定this
         let frame = () => {
+			let {color} = this.config
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
             for (let flake of this.snowflakes) {
                 flake.update(this.canvas.height)
                 this.ctx.save()
                 this.ctx.globalAlpha = flake.alpha
-                this.ctx.fillStyle = color
+                this.ctx.fillStyle = flake.color
                 this.ctx.beginPath()
                 this.ctx.arc(flake.x, flake.y, flake.radius,0,2*Math.PI)
                 this.ctx.closePath()
