51259db98e8392fa8dbaa68b0d56ef6add84bca4
QinXuYang <1446445040@qq.com>
2020-01-14 20:14:09 +0800

    修复翻转异常的问题

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-import '../css/main.css'
-import Snow from './snow'
-
-new Snow('#snow', {
-    image: ['./snow.png'], // 图片应该也可以拓展成传数组
-    radius: [10, 80],
-    color: ['red', 'orange', 'green', 'limegreen', 'blue', 'purple']
-})
